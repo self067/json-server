acd7dd4f380cc4588de948c61cc55bd3cc1117d7
self067 <seltor@mail.ru>
2020-08-23 08:54:37 +0300

    feat: add auth

--- server-bak.js
+++ server-bak.js
@@ -5,15 +5,6 @@ const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 3000;
 
 server.use(middlewares);
-
-server.use((req, res, next) => {
- if (isAuthorized(req)) { // add your authorization logic here
-   next(); // continue to JSON Server router
- } else {
-   res.sendStatus(401);
- }
-})
-
 server.use(router);
 
 server.listen(port);
