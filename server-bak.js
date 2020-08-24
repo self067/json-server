43c230904b492c7054222b7292cd8fab86e6b8d3
self067 <seltor@mail.ru>
2020-08-24 23:03:04 +0300

    feat: add auth

--- server-bak.js
+++ server-bak.js
@@ -1,10 +1,30 @@
 const jsonServer = require('json-server');
+console.dir( 'jsonServer:');
+console.dir( jsonServer);
+const jsonServerAuth = require('json-server-auth');
+
+console.dir( 'jsonServerAuth:');
+console.dir( jsonServerAuth);
+
 const server = jsonServer.create();
 const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 3000;
 
 server.use(middlewares);
+
+server.use((req, res, next) => {
+//console.log('req!!!!!!!!!!!!!!!!!!!!!!!!!!!!!', req);
+//console.log('res!!!!!!!!!!!!!!!!!!!!!!!!!!!!!', res);
+next(); // continue to JSON Server router
+
+//  if (isAuthorized(req)) { // add ygit statusour authorization logic here
+//    next(); // continue to JSON Server router
+//  } else {
+//    res.sendStatus(401);
+//  }
+})
+
 server.use(router);
 
 server.listen(port);
--- server.js
+++ server.js
@@ -1,23 +1,53 @@
-const jsonServer = require('json-server');
-const server = jsonServer.create();
-const router = jsonServer.router('db.json');
-const middlewares = jsonServer.defaults();
-const port = process.env.PORT || 3000;
+// const port = process.env.PORT || 4000;
+const port = 4000;
 
+const jsonServer = require('json-server');
+// console.log( 'jsonServer:',jsonServer);
+
+const auth = require('json-server-auth');
+// console.log( 'jsonServerAuth:',auth);
+
+const server = jsonServer.create();
+// console.log('server:',server);
+
+const router = jsonServer.router('db.json');
+// console.log('router:', router);
+
+server.db = router.db;
+
+const middlewares = jsonServer.defaults();
+// console.log('middlewares:',middlewares);
+
+// Add custom routes before JSON Server router
+// server.get('/echo', (req, res) => {
+//   res.jsonp(req.query)
+// });
 server.use(middlewares);
 
-server.use((req, res, next) => {
-console.log('req!!!!!!!!!!!!!!!!!!!!!!!!!!!!!', req);
-console.log('res!!!!!!!!!!!!!!!!!!!!!!!!!!!!!', res);
-next(); // continue to JSON Server router
+const rules = auth.rewriter({
+  // Permission rules
+  db: 640,
+  users: 600,
+  roles: 640,
+  tasks: 640,
+  // Other rules
+  '/posts/:category': '/posts?category=:category',
+});
+ 
+// You must apply the middlewares in the following order
+server.use(rules);
+server.use(auth);
 
+// server.use((req, res, next) => {
+//console.log('req!!!!!!!!!!!!!!!!!!!!!!!!!!!!!', req);
+//console.log('res!!!!!!!!!!!!!!!!!!!!!!!!!!!!!', res);
+// next(); // continue to JSON Server router
 //  if (isAuthorized(req)) { // add ygit statusour authorization logic here
 //    next(); // continue to JSON Server router
 //  } else {
 //    res.sendStatus(401);
 //  }
-})
+// });
 
 server.use(router);
-
 server.listen(port);
